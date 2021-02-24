a2bcf577a9279dc28c18f80f68abc2a5a20f02b6
jesusrda <jrdadb@gmail.com>
2021-02-24 11:15:46 -0400

    Cuestiones de estilo

--- Oraculo.hs
+++ Oraculo.hs
@@ -24,20 +24,24 @@ crearOraculo :: String -> Oraculo
 crearOraculo pred = Prediccion pred
 
 prediccion :: Oraculo -> String
-prediccion orac = case orac of Prediccion s -> s
-                               Pregunta _ _ -> error "El oraculo no es una predicción"
+prediccion oraculo = case oraculo of 
+                      Prediccion s -> s
+                      Pregunta _ _ -> error "El oraculo no es una predicción"
 
 pregunta :: Oraculo -> String
-pregunta orac = case orac of Pregunta s _ -> s
-                             Prediccion _ -> error "El oraculo no es una pregunta"
+pregunta oraculo = case oraculo of 
+                    Pregunta s _ -> s
+                    Prediccion _ -> error "El oraculo no es una pregunta"
 
 opciones :: Oraculo -> Opciones
-opciones orac = case orac of Pregunta _ m -> m
-                             Prediccion _ -> error "El oráculo no es una pregunta"
+opciones oraculo = case oraculo of 
+                    Pregunta _ m -> m
+                    Prediccion _ -> error "El oráculo no es una pregunta"
  
 respuesta :: Oraculo -> String -> Oraculo
-respuesta orac preg = case orac of Pregunta _ m -> m M.! preg
-                                   Prediccion _ -> error "El oráculo no es una pregunta"
+respuesta oraculo preg = case oraculo of 
+                          Pregunta _ m -> m M.! preg
+                          Prediccion _ -> error "El oráculo no es una pregunta"
 
 ramificar :: [String] -> [Oraculo] -> String -> Oraculo
 ramificar pregs oracs preg = Pregunta {getPregunta = preg
